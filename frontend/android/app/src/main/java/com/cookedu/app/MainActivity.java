6cb51c39da54f4485adc6bce57e0706bac996aba
CookEdu Developer <example@example.com>
2026-05-30 02:53:25 +0700

    Clear repository - remove all files

--- frontend/android/app/src/main/java/com/cookedu/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.cookedu.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- push_to_github.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title CookEdu Git Updater
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0push_to_github.ps1"
-pause
